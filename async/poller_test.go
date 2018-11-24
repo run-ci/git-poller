05fa226d265cfc4994cf0d70bbb333a1a0599c0d
Hugo Torres <hugo.torres1993@gmail.com>
2018-11-24 13:50:00 -0500

    Add test for getting pollers

--- async/poller_test.go
+++ async/poller_test.go
@@ -91,3 +91,50 @@ func TestPoolRemove(t *testing.T) {
 		}
 	}
 }
+
+func TestGetPollers(t *testing.T) {
+	errch := make(chan error)
+
+	pool := NewPool()
+
+	go func() {
+		err := pool.Run()
+		if err != nil {
+			errch <- err
+		}
+	}()
+
+	plr := &testPoller{
+		ch: make(chan struct{}),
+	}
+
+	plr.pollfn = func(ctx context.Context) error {
+		// This should just run until it's deleted.
+		for {
+			select {
+			case <-ctx.Done():
+				plr.ch <- struct{}{}
+			}
+		}
+	}
+
+	pool.AddPoller("test", plr)
+
+	plrs := pool.GetPollers()
+	if len(plrs) != 1 {
+		t.Fatalf("expected one poller after adding one poller, got %v", len(plrs))
+	}
+
+	if plrs[0] != "test" {
+		t.Fatalf("expected poller to have key 'test', got %v", plrs[0])
+	}
+
+	pool.DeletePoller("test")
+	// The poller will send on this as soon as it's killed.
+	<-plr.ch
+
+	plrs = pool.GetPollers()
+	if len(plrs) != 0 {
+		t.Fatalf("expected no poller after deleting one poller, got %v", len(plrs))
+	}
+}
